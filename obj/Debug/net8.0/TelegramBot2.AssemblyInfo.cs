05a836d757b33cf19d702da38e10a9bea958d2ad
simpluc <120049415+simpluc@users.noreply.github.com>
2024-11-17 19:35:26 +0100

    - full working version 1.0

--- obj/Debug/net8.0/TelegramBot2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TelegramBot2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramBot2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceae4d267835143649434b8c69eaacd358a2ba0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477e7290cd593b5cbbcb49c37f06a0f72c34575d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramBot2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramBot2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
