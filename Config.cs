1f6e1850c0061758fd3b3a19ff5d4ca2f3207134
Alex <asoreanu@movia.biz>
2024-11-18 15:00:18 +0100

    - Added Dockerfile and Readme file

--- Config.cs
+++ Config.cs
@@ -1,18 +1,18 @@
 
 class ConfigClass{
 
-    public static string api_key= "";
+    public static string api_key= "7621712307:AAFaYoM2JOhi9YjVYiunmdMLCnjQuzFxTIc";
     public static string Config(string what)
     {
         switch (what)
         {
-            case "api_id": return "";
-            case "api_hash": return "";
-            case "phone_number": return "";
+            case "api_id": return "20587725";
+            case "api_hash": return "00fbea4bc3d73d8f3ec935fe76bc8d1f";
+            case "phone_number": return "+393515698542";
             case "verification_code": Console.Write("Code: "); return Console.ReadLine()!;
             case "first_name": return "Stefan Alexandru";      // if sign-up is required
             case "last_name": return "Soreanu";        // if sign-up is required
-            case "password": return "";     // if user has enabled 2FA
+            case "password": return "password123";     // if user has enabled 2FA
             default: return null!;                  // let WTelegramClient decide the default config
         }
     }
---
+++ Readme.md
@@ -0,0 +1,7 @@
+<h1>Docker Installation</h1>
+<ul>
+    <li>Download the Dockerfile</li>
+    <li>Open terminal where the Dockerfile is</li>
+    <li>Use this command to make the image '  docker build -t telegrambot_byalex-image -f Dockerfile . '</li>
+    <li>Create a container ' docker create --name core-telegrambot_byalex telegrambot_byalex-image '</li>
+</ul>
