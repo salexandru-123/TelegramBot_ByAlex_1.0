57fbb7d9dfb05aba6a0dc4ebec0aa4ec6c61f257
Stefan Alexandru <73100457+salexandru-123@users.noreply.github.com>
2024-11-18 15:02:51 +0100

    Update Config.cs
    Removed private secret data

--- Config.cs
+++ Config.cs
@@ -1,18 +1,18 @@
 
 class ConfigClass{
 
-    public static string api_key= "7621712307:AAFaYoM2JOhi9YjVYiunmdMLCnjQuzFxTIc";
+    public static string api_key= "your_api_key";
     public static string Config(string what)
     {
         switch (what)
         {
-            case "api_id": return "20587725";
-            case "api_hash": return "00fbea4bc3d73d8f3ec935fe76bc8d1f";
-            case "phone_number": return "+393515698542";
+            case "api_id": return "your_app_api_id";
+            case "api_hash": return "your_app_api_hash";
+            case "phone_number": return "your_phone_with_prefix";
             case "verification_code": Console.Write("Code: "); return Console.ReadLine()!;
             case "first_name": return "Stefan Alexandru";      // if sign-up is required
             case "last_name": return "Soreanu";        // if sign-up is required
-            case "password": return "password123";     // if user has enabled 2FA
+            case "password": return "your_2FA_password";     // if user has enabled 2FA
             default: return null!;                  // let WTelegramClient decide the default config
         }
     }
